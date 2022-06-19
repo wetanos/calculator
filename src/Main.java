05932e45493b13e15c5a8a4cc937088621cb29ff
Kirill Vasilenko <wetanos@gmail.com>
2022-06-20 01:35:37 +0300

    Reverted changes with "method" variable's type

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Чтобы сложить числа - введите 1, чтобы умножить - 2");
-        String method = scanner.nextLine();
-        if (method == "1") {
+        int method = scanner.nextInt();
+        if (1 == method) {
+            System.out.println("Поочередно введите два целочисленных числа");
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             System.out.println("Результат сложения этих чисел: " + (a + b));
-        } else if (method == "2") {
+        } else if (method == 2) {
+            System.out.println("Поочередно введите два целочисленных числа");
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             System.out.println("Результат умножения этих чисел: " + (a * b));
